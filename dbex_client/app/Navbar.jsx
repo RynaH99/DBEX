4c4545ba7094a7c776d91d3ce12276a101118189
RynaH99 <166332195+RynaH99@users.noreply.github.com>
2024-06-02 20:39:17 -0500

    testing

--- dbex_client/app/Navbar.jsx
+++ dbex_client/app/Navbar.jsx
@@ -9,10 +9,10 @@ function Navbar() {
                     <h1 className='font-bold py-2 px-4 text-6xl text-white'>DBEX Tech</h1>
                 </div>
                 <ul className="flex space-x-4">
-                    <li><a href="/demo_table" className="text-white hover:text-gray-300">Demo Page</a></li>
-                    {/* <li><a href="/" className="text-white hover:text-gray-300">Home</a></li> */}
-                    
-                    <li><a href="/login_page" className='bg-white hover:bg-black-lightest text-black font-bold py-1 px-4 rounded border border-black-700 m-1 p-4'>Login</a></li>
+                    <li><a href="/demo_table" className="text-white font-bold hover:text-gray-300">Demo Page</a></li>
+                    <li><a href="/login_page" className='bg-black hover:text-gray-300 text-white font-bold py-1 px-4 '>Updates</a></li>
+                    <li><a href="/" className='bg-black hover:text-gray-300 text-white font-bold py-1 px-4 rounded border border-black-700 m-1'>Register</a></li>
+                    <li><a href="/login_page" className='bg-white hover:text-gray-700 text-black font-bold py-1 px-4 rounded border border-black-700 m-1 p-4'>Login</a></li>
                 </ul>
             </div>
         </nav>
