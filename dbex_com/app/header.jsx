77535895702b6d6876fa8e9b104124611c14d457
ethanson16 <et.hanson16@gmail.com>
2024-06-04 23:16:11 -0500

    f

--- dbex_com/app/header.jsx
+++ dbex_com/app/header.jsx
@@ -1,10 +1,12 @@
 import './CSS/header.css';
+import logo from './dbex-logo-1.png';
+
 
 export default function Header() {
     return (
         <div>
             <div class="header">
-            <a class="anchor-logo" href="https://dbextech.com/index.html"><img class="logo-pic" src="webpage-pics/DBEX  Logo 1.png" alt=""></img></a>
+            <a class="anchor-logo" href="https://dbextech.com/index.html"><img class="logo-pic" src='images/dbex-logo-1.png' alt=""></img></a>
             <div class="middle-header">
                 <a class="anchor-ind" href="https://dbextech.com/industries.html">Industries</a>
                 <a class="anchor-prod" href="https://dbextech.com/products.html">Products</a>
