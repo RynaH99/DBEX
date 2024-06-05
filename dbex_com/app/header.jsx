adb089d6e7ff64344bf37a6a312530a4cfdbdec8
ethanson16 <et.hanson16@gmail.com>
2024-06-04 22:03:48 -0500

    started next.js

---
+++ dbex_com/app/header.jsx
@@ -0,0 +1,19 @@
+import './CSS/header.css';
+
+export default function Header() {
+    return (
+        <div>
+            <div class="header">
+            <a class="anchor-logo" href="https://dbextech.com/index.html"><img class="logo-pic" src="webpage-pics/DBEX  Logo 1.png" alt=""></img></a>
+            <div class="middle-header">
+                <a class="anchor-ind" href="https://dbextech.com/industries.html">Industries</a>
+                <a class="anchor-prod" href="https://dbextech.com/products.html">Products</a>
+                <a class="anchor-TBD" href="https://dbextech.com/index.html">TBD</a>
+            </div>
+            <div class="right-header"><a class="live-demo" href="https://dbextech.com/demo.html">Go to Live Demo page</a></div>
+        </div>
+                
+        </div>
+        );
+    }
+
--- dbex_com/app/page.jsx
+++ dbex_com/app/page.jsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Header from './header.jsx';
 
 export default function Home() {
   return (
     <>
-    
+      <Header/>
     </>
   );
 }
