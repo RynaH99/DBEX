821e50fa415e027dc03517dccbbf23692df618ef
ethanson16 <155556623+ethanson16@users.noreply.github.com>
2024-06-05 21:59:13 -0500

    Delete dbex_com directory

--- dbex_com/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"707hDaKcVdZe1+DmMHB26NuRUaH27je7QqEQpvOVn2k=\"\n}"
--- dbex_com/app/page.jsx
+++
@@ -1,9 +0,0 @@
-import Header from './header.jsx';
-
-export default function Home() {
-  return (
-    <>
-      <Header/>
-    </>
-  );
-}
