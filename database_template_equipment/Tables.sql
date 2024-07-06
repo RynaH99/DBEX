3370669a9c917980d2e3e48ea6ab23cc7555a740
C-West8 <125926137+C-West8@users.noreply.github.com>
2024-07-06 11:08:35 -0500

    added some basic tables for equipment template

--- database_template_equipment/Tables.sql
+++ database_template_equipment/Tables.sql
@@ -9,7 +9,8 @@
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 IF NOT EXISTS(SELECT * FROM sys.schemas WHERE NAME = N'enum')       EXEC('CREATE SCHEMA [enum]')
-IF NOT EXISTS(SELECT * FROM sys.schemas WHERE NAME = N'products')   EXEC('CREATE SCHEMA [products]')
+IF NOT EXISTS(SELECT * FROM sys.schemas WHERE NAME = N'equipment')  EXEC('CREATE SCHEMA [equipment]')
+-- IF NOT EXISTS(SELECT * FROM sys.schemas WHERE NAME = N'products')   EXEC('CREATE SCHEMA [products]')
 IF NOT EXISTS(SELECT * FROM sys.schemas WHERE NAME = N'invoices')   EXEC('CREATE SCHEMA [invoices]')
 
 
@@ -40,26 +41,43 @@ Idea: Make an App later
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-CREATE TABLE enum.company
-(
-    ID_auto INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-    company_name    VARCHAR(MAX),
+CREATE TABLE IF NOT EXISTS enum.company (
+    ID_auto         SERIAL PRIMARY KEY,
+    company_name    TEXT,
+    company_address TEXT --TODO: consider changing this to a more fitted data type and proper addressing. 
 )
 
 
---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- Create Product Tables
---    _____                _         _____               _            _     _______    _     _           
---   / ____|              | |       |  __ \             | |          | |   |__   __|  | |   | |          
---  | |     _ __ ___  __ _| |_ ___  | |__) | __ ___   __| |_   _  ___| |_     | | __ _| |__ | | ___  ___ 
---  | |    | '__/ _ \/ _` | __/ _ \ |  ___/ '__/ _ \ / _` | | | |/ __| __|    | |/ _` | '_ \| |/ _ \/ __|
---  | |____| | |  __/ (_| | ||  __/ | |   | | | (_) | (_| | |_| | (__| |_     | | (_| | |_) | |  __/\__ \
---   \_____|_|  \___|\__,_|\__\___| |_|   |_|  \___/ \__,_|\__,_|\___|\__|    |_|\__,_|_.__/|_|\___||___/
---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Create Equipment Tables
+--    _____                _         ______            _                            _     _______    _     _           
+--   / ____|              | |       |  ____|          (_)                          | |   |__   __|  | |   | |          
+--  | |     _ __ ___  __ _| |_ ___  | |__   __ _ _   _ _ _ __  _ __ ___   ___ _ __ | |_     | | __ _| |__ | | ___  ___ 
+--  | |    | '__/ _ \/ _` | __/ _ \ |  __| / _` | | | | | '_ \| '_ ` _ \ / _ \ '_ \| __|    | |/ _` | '_ \| |/ _ \/ __|
+--  | |____| | |  __/ (_| | ||  __/ | |___| (_| | |_| | | |_) | | | | | |  __/ | | | |_     | | (_| | |_) | |  __/\__ \
+--   \_____|_|  \___|\__,_|\__\___| |______\__, |\__,_|_| .__/|_| |_| |_|\___|_| |_|\__|    |_|\__,_|_.__/|_|\___||___/
+--                                            | |       | |                                                            
+--                                            |_|       |_|                                                            
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+CREATE TABLE IF NOT EXISTS equipment.family (
+    ID_auto     SERIAL PRIMARY KEY,
+    family_name TEXT,
+    parts_used  TEXT
+);
 
 
+CREATE TABLE IF NOT EXISTS equipment.instance (
+    ID_auto     SERIAL PRIMARY KEY,
+    device_name TEXT,
+    family_ID   INTEGER,
+    company_ID  INTEGER,
+    CONSTRAINT equipment_instance_family_ID  FOREIGN KEY (family_ID) REFERENCES equipment.family(ID_auto),
+    CONSTRAINT equipment_instance_company_ID FOREIGN KEY (company_ID) REFERENCES enum.company(ID_auto)
+);
 
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Create Invoice Tables
 --    _____                _         _____                 _            _______    _     _           
 --   / ____|              | |       |_   _|               (_)          |__   __|  | |   | |          
 --  | |     _ __ ___  __ _| |_ ___    | |  _ ____   _____  _  ___ ___     | | __ _| |__ | | ___  ___ 
