657eb28b6fa5d57513681e650d5e5b5e4828c100
ethanson16 <et.hanson16@gmail.com>
2024-07-06 20:09:19 -0500

    more table updates

--- dbex-react-app/src/table.jsx
+++ dbex-react-app/src/table.jsx
@@ -7,54 +7,59 @@ function Table() {
   return (
     <div>
       <table className="main-table">
-        <tr className="basic-row">
-          <th>ID</th>
-          <th>Date</th>
-          <th>
-            Workout<button>+</button>
-          </th>
-          <th style={{ width: "200px" }}>Set x Reps</th>
-        </tr>
-        <tr className="basic-row">
-          <td>1</td>
-          <td>{Date()}</td>
-          <td>Dropdown</td>
-          <td>
-            <div className="saved-rep">{workoutRep1[0]}</div>
-            <div className="saved-rep">{workoutRep1[1]}</div>
-            <div className="saved-rep">{workoutRep1[2]}</div>
-            <div className="saved-rep">{workoutRep1[3]}</div>
-          </td>
-        </tr>
-        <tr className="basic-row">
-          <td>1</td>
-          <td>{Date()}</td>
-          <td>Dropdown</td>
-          <td>
-            <div className="saved-rep">{workoutRep2[0]}</div>
-            <div className="saved-rep">{workoutRep2[1]}</div>
-            <div className="saved-rep">{workoutRep2[2]}</div>
-            <div className="saved-rep">{workoutRep2[3]}</div>
-          </td>
-        </tr>
-        <tr className="basic-row">
-          <td>2</td>
-          <td>{Date()}</td>
-          <td>Choose</td>
-          <td>
-            <div className="current-rev-div">
-              <input className="rep-input" />
-              <input className="rep-input" />
-              <input className="rep-input" />
-              <input className="rep-input" />
-              <button className="add-rep-button">+</button>
-            </div>
-          </td>
-        </tr>
+        <tbody>
+          <tr style={{ height: "40px" }}>
+            <th>ID</th>
+            <th>Date</th>
+            <th style={{ width: "100px", paddingRight: "0px" }}>
+              <div style={{ position: "relative" }}>
+                <div style={{ display: "inline-block" }}>Workout</div>
+                <button style={{ position: "absolute", right: "0px" }}>
+                  +
+                </button>
+              </div>
+            </th>
+            <th style={{ width: "200px" }}>Set x Reps</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>{Date()}</td>
+            <td>Dropdown</td>
+            <td>
+              <div className="saved-rep">{workoutRep1[0]}</div>
+              <div className="saved-rep">{workoutRep1[1]}</div>
+              <div className="saved-rep">{workoutRep1[2]}</div>
+              <div className="saved-rep">{workoutRep1[3]}</div>
+            </td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>{Date()}</td>
+            <td>Dropdown</td>
+            <td>
+              <div className="saved-rep">{workoutRep2[0]}</div>
+              <div className="saved-rep">{workoutRep2[1]}</div>
+              <div className="saved-rep">{workoutRep2[2]}</div>
+              <div className="saved-rep">{workoutRep2[3]}</div>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>{Date()}</td>
+            <td>Choose</td>
+            <td>
+              <div className="current-rev-div">
+                <input className="rep-input" />
+                <input className="rep-input" />
+                <input className="rep-input" />
+                <input className="rep-input" />
+                <button className="add-rep-button">+</button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
       </table>
-      <button className="save-button" onClick={alert("Try Again")}>
-        SAVE
-      </button>
+      <button className="save-button">SAVE</button>
     </div>
   );
 }
