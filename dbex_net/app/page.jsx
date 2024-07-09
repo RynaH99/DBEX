ab7b7fc8dec9c73d66e36a9338def3800e9a09fc
RynaH99 <166332195+RynaH99@users.noreply.github.com>
2024-07-08 21:07:55 -0500

    update folders

--- dbex_net/app/page.jsx
+++ dbex_net/app/page.jsx
@@ -5,6 +5,7 @@ import LoginNavbar from '@/app/components/loginNavbar.jsx'
 
 export default function Home() {
   return (
+
     <div className="bg-white min-h-screen flex flex-col justify-center">
       <LoginNavbar />
       <div className="flex justify-between w-full mt-8">
